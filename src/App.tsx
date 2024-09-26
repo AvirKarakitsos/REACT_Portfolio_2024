3f14b7995c844ac4770c4054a0bb35e6e258540e
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-09-26 21:29:20 +0200

    feat: Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { BrowserRouter} from 'react-router-dom';
 import { ThemeProvider } from './utils/context/ThemeContext.js';
 import { LanguageProvider } from './utils/context/LanguageContext.js';
 
 import Header from './Components/Header.js';
+import Footer from './Components/Footer.js';
 
 function App() {
 
   return (
-    <BrowserRouter>
-			<ThemeProvider>
-				<LanguageProvider>
-					<Header/>
-				</LanguageProvider>
-			</ThemeProvider>
-		</BrowserRouter>
+	<ThemeProvider>
+		<LanguageProvider>
+			<Header/>
+			<Footer/>
+		</LanguageProvider>
+	</ThemeProvider>
   )
 }
 
